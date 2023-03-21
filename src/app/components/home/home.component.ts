258067b3b58fbe540485eac2ff6c60b16fe4d2bc
Andrei Amariei <amarieiandrei60@gmail.com>
2023-03-21 14:40:54 +0200

    21-Mar-2023-DeleteNonImportantFiles

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,7 +2,8 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-home',
-  templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss'],
+  template: `<app-header></app-header>
+    <app-main></app-main>
+    <app-footer></app-footer>`,
 })
 export class HomeComponent {}
